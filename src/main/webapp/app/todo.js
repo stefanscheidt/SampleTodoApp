fb940ab70568553398c51c9e47c2d74705d5edf4
Stefan Scheidt <stefan.scheidt@opitz-consulting.com>
2012-05-08 09:01:59 +0200

    Cleanup.

--- src/main/webapp/app/todo.js
+++ src/main/webapp/app/todo.js
@@ -1,17 +1,21 @@
 function TodoController($scope) {
     $scope.todos = [
         {text:"Learn Angular", done:false},
-        {text:"Write Todo App", done:false}];
+        {text:"Write Todo App", done:false}
+    ];
+
     $scope.total = function () {
         return $scope.todos.length;
     }
+
     $scope.add = function() {
         $scope.todos.push({text:$scope.inputTodo, done:false});
         $scope.inputTodo = "";
     }
+
     $scope.cleanUp = function() {
         $scope.todos = _.filter($scope.todos, function(todo) {
             return !todo.done;
-        })
+        });
     }
 }
